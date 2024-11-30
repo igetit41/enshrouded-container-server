d74c2d22a67663bd60377045feece09a052bf5f9
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-11-30 13:12:51 -0800

    bad syntax

--- enshrouded/enshrouded.sh
+++ enshrouded/enshrouded.sh
@@ -4,4 +4,4 @@ git -C /home/d3f1l3/enshrouded-container-server pull origin main
 chmod +x /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.sh
 cp /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.service /etc/systemd/system/enshrouded.service
 
-docker-compose up -d
+docker compose up -d
