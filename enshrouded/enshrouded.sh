6cb38229ef324263e70952724d16aef97dc49371
josterhaus-unity <148155151+josterhaus-unity@users.noreply.github.com>
2024-11-29 19:20:24 -0800

    git reset

--- enshrouded/enshrouded.sh
+++ enshrouded/enshrouded.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git -C /home/d3f1l3/enshrouded-container-server reset --hard
 git -C /home/d3f1l3/enshrouded-container-server pull origin main
 chmod +x /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.sh
 cp /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.service /etc/systemd/system/enshrouded.service
