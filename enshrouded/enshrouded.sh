268d1225fcf0a462c20adf90b356525686f14ff2
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-11-30 14:58:01 -0800

    pathing

--- enshrouded/enshrouded.sh
+++ enshrouded/enshrouded.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-git -C /home/d3f1l3/enshrouded-container-server reset --hard
-git -C /home/d3f1l3/enshrouded-container-server pull origin main
-chmod +x /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.sh
-cp /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.service /etc/systemd/system/enshrouded.service
+export GITPATH=/home/d3f1l3/enshrouded-container-server/
+export GITSERVICEPATH=enshrouded/
 
-docker compose up -d
+git -C ${$GITPATH} reset --hard
+git -C ${$GITPATH} pull origin main
+chmod +x ${$GITPATH}${$GITSERVICEPATH}enshrouded.sh
+cp ${$GITPATH}${$GITSERVICEPATH}enshrouded.service /etc/systemd/system/enshrouded.service
+
+docker compose --file ${$GITPATH}${$GITSERVICEPATH}compose.yaml up -d
