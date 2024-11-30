57d3e760924b55936d80a5c40d17540df7825a59
josterhaus-unity <148155151+josterhaus-unity@users.noreply.github.com>
2024-11-29 19:10:03 -0800

    chmod

--- enshrouded/enshrouded.sh
+++ enshrouded/enshrouded.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git -C /home/d3f1l3/enshrouded-container-server pull origin main
+chmod +x /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.sh
+cp /home/d3f1l3/enshrouded-container-server/enshrouded/enshrouded.service /etc/systemd/system/enshrouded.service
 
 docker compose up -d
